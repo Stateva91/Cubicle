c6119329c899f25ab150e247523c3e8283908f93
Snezhana Stateva <114188863+Stateva91@users.noreply.github.com>
2023-10-18 15:32:45 +0200

    add accessory manager

--- src/managers/accessoryManager.js
+++ src/managers/accessoryManager.js
@@ -1,1 +1,3 @@
-exports.create=(accessoryData)=> console.log(accessoryData);
+const Accessory = require('../models/accessory');
+
+exports.create = (accessoryData) => Accessory.create(accessoryData);
